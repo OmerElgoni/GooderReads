72fa9874643aca383fe4053e24a3306d80b3f5a9
Omer Elgoni <OmerFaroug.Elgoni@vcontractor.co.za>
2021-05-20 12:18:39 +0200

    Fleshed out book details page slightly

---
+++ public/js/bookDetails.js
@@ -0,0 +1,50 @@
+function setTitle(title) {
+    const element = document.getElementById("bookTitle");
+    element.textContent = title;
+  }
+
+  function setCoverImage(coverImageUrl, title) {
+    const element = document.getElementById("coverImage");
+    element.src = coverImageUrl ? coverImageUrl : `https://via.placeholder.com/200x250.?text=${title}jpg`;
+}
+
+function setRatings(positiveRatings, negativeRatings) {
+    document.getElementById("positiveReviews").textContent = positiveRatings + " positive reviews";
+    document.getElementById("negativeReviews").textContent = negativeRatings + " negative reviews";
+}
+function setAuthors(authors) {
+    const element = document.getElementById("bookAuthor");
+    let authorText =`by ${authors[0].first_name} ${authors[0].last_name}`;
+    for (let index = 1; index < authors.length; index++) {
+
+        const nextAuthor = `${authors[i].first_name} ${authors[i].last_name}`;
+        authorText = authorText + ' and ' + nextAuthor
+        
+    }
+    element.textContent = authorText;
+  }
+  
+async function setDetails() {
+    const APIEndpoint = "https://grad-gooder-reads-database.herokuapp.com/api/";
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const bookId = urlParams.get("id");
+    console.log("bookId", bookId);
+  
+    let authorQueryResult = fetch(`${APIEndpoint}books/${bookId}/author`);
+    const bookQueryResult = await (
+      await fetch(`${APIEndpoint}books/${bookId}`)
+    ).json();
+    console.log("bookQueryResult", bookQueryResult)
+    authorQueryResult = await (
+        await authorQueryResult
+      ).json();
+    console.log("authorQueryResult", authorQueryResult)
+    setTitle(bookQueryResult.title);
+    setCoverImage(bookQueryResult.cover_image, bookQueryResult.title);
+    setAuthors(authorQueryResult);
+    setRatings(bookQueryResult.positive_rating, bookQueryResult.negative_rating);
+  }
+  
+  
+  window.onload = setDetails();
