6867efcadb1bcc63574c6794a6a52e50c456b5f4
Thatos <thatos@bbd.co.za>
2021-05-20 20:22:28 +0200

    modified code on readlist page

--- public/js/readlist.js
+++ public/js/readlist.js
@@ -13,14 +13,6 @@ async function queryUserAPI() {
     const queryResult = await (await fetch(`${APIEndpoint}users/${userId}`).then(response => response.json()));
 
     console.log(queryResult);
-
-    // const name = document.getElementById("user-profile-name");
-    // const email = document.getElementById("user-profile-email");
-
-    // name.innerHTML = queryResult.first_name + ' ' + queryResult.last_name;
-
-    // // only show email if authenticated?
-    // email.innerHTML = queryResult.email_address;
 }
 async function getPastBooksData() {
     const queryResult = await (await fetch(`${APIEndpoint}users/${userId}/readlist`).then(response => response.json()));
@@ -37,19 +29,18 @@ async function getPastBooksData() {
 
     var books = Array(12).fill(0);
     var pages = Array(12).fill(0);
-    var cover_art = "images/FC9781491927182.png";
 
+    var cover_art = "";
     queryResult[0].past_books.forEach((pastBook, i) => {
         var date = new Date(pastBook.user_past_book.date_completed.replace(' ', 'T'));
         console.log({ pastBook });
 
-        // read_list += "" + '<section class="book-info-block"><section class="image-container"><img id=" book-art " class="image" src="' +
-        //     pastBook.cover_art + '"/></section></section>' + '';
+        cover_art = pastBook.cover_art;
 
         read_list += "" + '<section class="book-info-block">' + '' +
             '<section class="image-container">' + '' +
             // '<img id=" book-art " class="image" src="' + pastBook.cover_art + '"/>' +
-            '<img id=" book-art " class="image" src="images/FC9781491927182.png"/>' +
+            '<img id=" book-art " class="image" src="' + cover_art + '"/>' +
             '' +
             '</section>' +
             '<section class="book-info ">' +
@@ -60,23 +51,7 @@ async function getPastBooksData() {
             '</section>' +
             ' </section>' + '';
 
-        //     read_list += "<article>" +
-        //         '<section class="book-title">' +
-        //         pastBook.title +
-        //         "</section>" +
-        //         '\"' +
-        //         pastBook.user_past_book.review +
-        //         '\"' +
-        //         "</article>"
-        // }
-
-
-        // if (date > new Date().setMonth(new Date().getMonth() - 12)) {
-        //     books[date.getMonth()] += 1;
-        //     pages[date.getMonth()] += pastBook.pages || 0;
-        // }
     });
 
     bookListSection.innerHTML = read_list;
-    //setUpCharts(books, pages);
 }
