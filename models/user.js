d39459277cbc9b69490234eef0b3fabd2bcdd068
Omer Elgoni <OmerFaroug.Elgoni@vcontractor.co.za>
2021-05-20 16:18:05 +0200

    Changed wantlist to dog

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ module.exports = (sequelize, DataTypes) => {
     User.belongsToMany(models["book"], {
       through: models["wishlist"],
       foreignKey: "user_id",
-      as: "wantlist",
+      as: "dog",
     });
   };
   //
