05d1b56b32397aec3350ba5ffede2f57b1c769a1
VF-ROOT\pilkm001 <MatthewRichard.Pilkington@vcontractor.co.za>
2021-05-20 11:10:45 +0200

    Refactored css and responsiveness. Fixed glitch on autocomplete

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -10,7 +10,7 @@ export function autocomplete(inputElement, autocompleteOptions) {
       if (!autocompleteOptions.includes(inputElement.value)) {
         inputElement.value = "";
       }
-    }, 100);
+    }, 200);
   });
 
   inputElement.addEventListener("input", showPopUp);
@@ -92,12 +92,11 @@ export function autocomplete(inputElement, autocompleteOptions) {
         !val ||
         option.toUpperCase().includes(val.toUpperCase())
       ) {
-        const matchingOption = document.createElement("DIV");
+        const matchingOption = document.createElement("div");
 
         matchingOption.innerHTML = option;
 
-        matchingOption.innerHTML +=
-          "<input type='hidden' value='" + option + "'>";
+        matchingOption.innerHTML += `<input type='hidden' value="${option}">`;
 
         matchingOption.addEventListener("click", function () {
           inputElement.value =
