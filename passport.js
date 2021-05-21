47bd86f1a7941ee8be1687e4186cb03d49e4d3b9
Malbogle <josephjasson22@gmail.com>
2021-05-21 08:40:48 +0200

    added auth stuff

--- passport.js
+++ passport.js
@@ -13,13 +13,12 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
   clientID: '1095537354823-52j5sogbp3gs0odm4fafu5lncq8dsm78.apps.googleusercontent.com',
   clientSecret: 'VepOmfAl_U6skYlif14bdgwZ',
-  callbackURL: "http://localhost:4000/auth/google/callback"
+  callbackURL: "http://localhost:4000/auth/google/callback" //needs to change when on heroku
 },
 async function(accessToken, refreshToken, user, done){
-  console.log(user.name.familyName)
-  console.log(user.name.givenName)
+
   const response = await (await fetch(`${APIEndpoint}/users/find/${user.emails[0].value}`)).json()
- 
+  console.log(response)
   if(response === null){
     data = {
       email_address: user.emails[0].value,
